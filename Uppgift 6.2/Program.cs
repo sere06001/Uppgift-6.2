b39320d0fa0da50073722725766a0b1ad85fa8e5
Kreedzz <sean.banan420@gmail.com>
2024-02-16 15:10:51 +0100

    Add project files.

---
+++ Uppgift 6.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace uppgift6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Tal 1: ");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.Write("Tal 2: ");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(StörstaTal(tal1, tal2));
+            Console.ReadKey();
+        }
+        static int StörstaTal(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+            else if (tal2>tal1)
+            {
+                return tal2;
+            }
+            else
+            {
+                return tal1+tal2;
+            }
+        }
+    }
+}
